05bac8c906e6c0764af61218ba77e10033b7e731
Ugur Öz <diyetisyenuguroz@gmail.com>
2022-03-14 08:33:58 +0100

    FunktioniertLast1

--- src/main/java/com/ugur/ReverseController.java
+++ src/main/java/com/ugur/ReverseController.java
@@ -1,17 +1,13 @@
 package com.ugur;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.RequestDispatcher;
@@ -20,13 +16,9 @@ import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-
 @Controller
 public class ReverseController {
-
     String showProblem;
-
     @GetMapping
     public String getProblem(Model model) {
         model.addAttribute("saveProblemForm", new ProblemForm());
@@ -50,11 +42,8 @@ public class ReverseController {
 
         VerschlimmerungForm newVerschlimmerungForm = new VerschlimmerungForm();
         newVerschlimmerungForm.setProblem_id(verschlimmerungForm.getProblem_id());
-
         String saveSQL = "INSERT INTO WORSENING(description, problem_id) VALUES (?,?)";
         jdbcTemplate.update(saveSQL, verschlimmerungForm.getDescription(), verschlimmerungForm.getProblem_id());
-
-
         List<VerschlimmerungForm> verschlimmerungFormList = jdbcTemplate.query("SELECT * FROM WORSENING WHERE worsening.problem_id = " + verschlimmerungForm.getProblem_id(), new VerschlimmerungRowMapper());
         model.addAttribute("verschlimmerungFormList", verschlimmerungFormList);
         model.addAttribute("problemText", showProblem);
@@ -66,9 +55,7 @@ public class ReverseController {
     public String saveVerschlimmerungForm(Model model, @RequestParam int problemId) {
         List<VerschlimmerungForm> verschlimmerungFormList = jdbcTemplate.query("SELECT * FROM WORSENING WHERE worsening.problem_id = " + problemId, new VerschlimmerungRowMapper());
         model.addAttribute("verschlimmerungFormList", verschlimmerungFormList);
-
         model.addAttribute("problemText", showProblem);
-
         VerschlimmerungForm newVerschlimmerungForm = new VerschlimmerungForm();
         newVerschlimmerungForm.setProblem_id(problemId);
         model.addAttribute("saveVerschlimmerungForm", newVerschlimmerungForm);
@@ -89,7 +76,6 @@ public class ReverseController {
 
     @PostMapping("/losung")
     public String getLosungForm(Model model, LosungForm losungForm, String problemId) {
-        System.out.println(losungForm);
 
         String saveSQL = "INSERT INTO SOLUTIONS(description, WORSENING_ID) VALUES (?,?)";
         jdbcTemplate.update(saveSQL, losungForm.getDescription(), losungForm.getWorsening_id());
@@ -117,7 +103,6 @@ public class ReverseController {
 
         return "ansicht";
     }
-
     @Bean
     public FlywayMigrationStrategy repairFlyway() {
         return flyway -> {
@@ -127,19 +112,15 @@ public class ReverseController {
             flyway.migrate();
         };
     }
-
     SimpleJdbcInsert simpleJdbcInsert;
-
     @Autowired
     public ReverseController(DataSource dataSource) {
         simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
                 .withTableName("PROBLEM").usingGeneratedKeyColumns("id");
 
     }
-
     @Autowired
     private JdbcTemplate jdbcTemplate;
-
     public long saveProblemToDbankReturnId(ProblemForm problemForm) {
         Map<String, Object> parameters = new HashMap<>(1);
         parameters.put("description", problemForm.getDescription());
